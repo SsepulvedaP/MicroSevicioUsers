3171ab66666a974dfbef7e352de7a8806c9f09eb
Sebastian Sepulveda Patiño <sebas20112322@gmail.com>
2024-08-31 17:14:22 -0500

    Creacion del microservicio usuarios

---
+++ src/main/java/com/MicroService/MicroServiceUsers/MicroServiceUsersApplication.java
@@ -0,0 +1,13 @@
+package com.MicroService.MicroServiceUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MicroService/MicroServiceUsers/MicroServiceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroService.MicroServiceUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
